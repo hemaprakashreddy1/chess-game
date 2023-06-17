a67ab07a5359b6e7342bce395898db8457693471
Hema Prakash <hemaprakashreddy1@gmail.com>
2023-06-17 20:11:24 +0530

    added command line args

--- chess.c
+++ chess.c
@@ -2172,9 +2172,10 @@ void init_new_game()
     init_rook_info();
 }
 
-int main()
+int main(int argc, char **argv)
 {
-    FILE *file = fopen("checkmated_20k.txt", "r");
+    char *file_name = argv[1];
+    FILE *file = fopen(file_name, "r");
     if(!file)
     {
         printf("error opening the file\n");
